2b3456fcc5f9c2010b90435fd81e022fd1528239
AR <techrebel2723@gmail.com>
2021-07-10 00:40:44 +0000

    css changes

--- app/theme.config.js
+++ app/theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem'}}>
-     © divvyr <time>{YEAR}</time>
+     © TotallyUsefulAPI <time>{YEAR}</time>
       <a href="/feed.xml">RSS Feed</a>
       <style jsx>{`
         a {
